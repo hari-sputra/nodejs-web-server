f76f17035f9f98fac4855aa3ab2d0f45ee52f4ac
Hari Saputra <hari.saputra.dev@gmail.com>
2022-01-04 12:35:13 +0700

    nodejs web server

---
+++ server.js
@@ -0,0 +1,79 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+
+
+
+    // response
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'NodeJs');
+
+    // mengambil properti url dari request
+    // request
+    const { method, url } = request;
+
+    if (url === '/') {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: "This is the homepage",
+            }));
+        } else {
+            response.statusCode = 400;
+             response.end(JSON.stringify({
+                message: `Page cannot be accessed with ${method} request`,
+            }));
+        }
+        
+    } else if (url === '/about') {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: "Hello, This is the About Page",
+            }));
+            
+        } else if (method === 'POST') {
+            let body = [];
+            
+            request.on('data', (chunk) => {
+            body.push(chunk);
+            });
+
+            request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            const { name } = JSON.parse(body);
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: `Hello, ${name}! This is the About Page`,
+            }));
+            
+           
+        });
+
+            
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: `Page cannot be accessed with ${method} request`,
+            }));
+           
+        }
+
+    } else {
+        response.statusCode = 404;
+        response.end(JSON.stringify({
+            message: "Page Not Found!",
+        }));
+    }
+    
+
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+    console.log(`Server running on http://${host}:${port}`);
+});
